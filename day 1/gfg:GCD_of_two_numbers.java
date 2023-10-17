87338fd70688936cf0ffd7d91436d9614080467e
Shreyashi Singh <123365942+shreyashi1323@users.noreply.github.com>
2023-10-17 18:14:22 +0530

    gfg:GCD_of_two_numbers

---
+++ day 1/gfg:GCD_of_two_numbers.java
@@ -0,0 +1,16 @@
+class Solution
+{
+    public int gcd(int A , int B) 
+    { 
+       if(B==0)
+       return A;
+       
+       if(A==0)
+       return B;
+       
+       if(A>B)
+       return gcd(B, A%B);
+       else
+       return gcd(A, B%A);
+    } 
+}
