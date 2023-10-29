edaa39b7e67ba32100a7dc37e7923c5c7271f47b
Shreyashi Singh <123365942+shreyashi1323@users.noreply.github.com>
2023-10-29 17:11:19 +0530

    GFG_second largest

---
+++ day 13/GFG_second largest.java
@@ -0,0 +1,17 @@
+class Solution {
+    int print2largest(int arr[], int n) {
+        int largest = -1;
+        int secondLargest = -1;
+
+        for (int i = 0; i < n; i++) {
+            if (arr[i] > largest) {
+                secondLargest = largest;
+                largest = arr[i];
+            } else if (arr[i] > secondLargest && arr[i] != largest) {
+                secondLargest = arr[i];
+            }
+        }
+
+        return secondLargest;
+    }
+}
