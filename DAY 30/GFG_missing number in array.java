fbba1fc2bc09609f9dbf86d6c0e5af889db1f2cb
Shreyashi Singh <123365942+shreyashi1323@users.noreply.github.com>
2023-11-15 11:21:00 +0530

    GFG_missing number in array

--- DAY 30/GFG_missing number in array.java
+++ DAY 30/GFG_missing number in array.java
@@ -5,8 +5,8 @@ class Solution {
 
         // Calculate the sum of the given array
         int actualSum = 0;
-        for (int num : array) {
-            actualSum += num;
+        for (int i=0;i<=array.length;i++) {
+            actualSum += array[i];
         }
 
         // The missing number is the difference between the expected sum and the actual sum
