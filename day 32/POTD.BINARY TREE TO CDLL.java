1bbfd46cf78357c66cbad6bb31ff4ea2678d41e1
Shreyashi Singh <123365942+shreyashi1323@users.noreply.github.com>
2023-11-17 18:35:48 +0530

    POTD.BINARY TREE TO CDLL

---
+++ day 32/POTD.BINARY TREE TO CDLL.java
@@ -0,0 +1,31 @@
+class Solution
+{
+  public:
+    //Function to convert binary tree into circular doubly linked list.
+    Node *head = NULL;
+    Node *prev = NULL;
+    void inorder(Node *curr){
+        if(curr == NULL){
+            return;
+        }
+        inorder(curr->left);
+        if(head == NULL){
+            head = prev= curr;
+            
+        }
+        else{
+            prev->right = curr;
+            curr->left = prev;
+        }
+        prev = curr;
+        inorder(curr->right);
+    }
+    Node *bTreeToCList(Node *root)
+    {
+    //add code here.
+        inorder(root);
+        head->left = prev;
+        prev->right = head;
+        return head;
+    }
+};
