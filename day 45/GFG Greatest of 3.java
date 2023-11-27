870b4760f74dc0161a7408059d205f7385942a24
Shreyashi Singh <123365942+shreyashi1323@users.noreply.github.com>
2023-11-27 19:09:34 +0530

    DAY 45

---
+++ day 45/GFG Greatest of 3.java
@@ -0,0 +1,17 @@
+// User function Template for C++
+
+class Solution {
+  public:
+    int greatestOfThree(int a, int b, int c) {
+        // code here
+        if(a>b && a>c)
+        return a;
+        
+        else if (b>a && b> c)
+        return b;
+        
+        else
+        return c;
+   
+    }
+};
