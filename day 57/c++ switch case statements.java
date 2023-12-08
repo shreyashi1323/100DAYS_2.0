a4957cf494c749241e045c51f97632474b2f95b5
Shreyashi Singh <123365942+shreyashi1323@users.noreply.github.com>
2023-12-08 12:21:46 +0530

    c++ switch case statements.java

---
+++ day 57/c++ switch case statements.java
@@ -0,0 +1,40 @@
+class Solution{
+    static String isInRange(int n){
+        // code here
+        switch(n){
+            case(1):
+            return "one"; 
+            
+            case(2):
+            return "two"; 
+            
+            case(3):
+           return "three"; 
+            
+            case(4):
+            return "four"; 
+            
+            case(5):
+            return "five"; 
+            
+            case(6):
+            return "six"; 
+            
+            case(7):
+            return "seven"; 
+            
+            case(8):
+            return "eight"; 
+            
+            case(9):
+             return "nine"; 
+            
+            case(10):
+            return "ten";
+            
+            default:
+             return "not in range";
+            
+        }
+    }
+}
