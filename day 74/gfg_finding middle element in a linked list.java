b8ad248249a42998c1a97344adfba8e7b9cf6597
Shreyashi Singh <123365942+shreyashi1323@users.noreply.github.com>
2023-12-25 02:18:51 +0530

    gfg_finding middle element in a linked list.java

---
+++ day 74/gfg_finding middle element in a linked list.java
@@ -0,0 +1,14 @@
+
+class Solution {
+    int getMiddle(Node head) {
+        Node fast = head;
+        Node slow = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        return slow.data;
+    }
+}
