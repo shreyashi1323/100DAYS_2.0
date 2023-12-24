76f984372f27d85dc2445ad5be49f29b2c65102a
Shreyashi Singh <123365942+shreyashi1323@users.noreply.github.com>
2023-12-24 18:42:03 +0530

    leetcode_27 Remove element.java

--- day 73/leetcode_27 Remove element.java
+++ day 73/leetcode_27 Remove element.java
@@ -1,7 +1,6 @@
 class Solution {
     public int removeElement(int[] nums, int val) {
         int n=nums.length;
-        int [] ans= new int [n];
         int k=0;
         for(int i=0;i<n;i++){
             if(nums[i]!=val){
